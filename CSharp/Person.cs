2752b29bfe88be84cb4c31fa4d7732e5ecc26a4b
Zachary Gillis <zgillis11@gmail.com>
2018-10-05 21:26:08 +0000

    Upload #1

---
+++ CSharp/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+		// Two variables that will make up a combined property
+		private string firstName;
+		private string lastName;
+
+        // Private background field that will be accessed
+        // through the 'NetWorth' property
+		decimal netWorth;
+
+        public Person(string first, string last)
+		{
+			firstName = first;
+			lastName = last;
+		}
+
+        // Property created from two private fields
+		public string Name => $"{firstName} {lastName}";
+
+        // Public property using private variable for value
+        public decimal NetWorth
+		{
+			get => netWorth;
+			set => netWorth = value;
+		}
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properties
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			Person bob = new Person("Robert", "McAllister");
+			Console.WriteLine(bob.Name);
+
+			bob.NetWorth = 160020.23m;
+			Console.WriteLine(string.Format("{0}'s net worth is: ${1}", bob.Name, bob.NetWorth));
+        }
+    }
+}
